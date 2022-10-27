2894e7919bc39b52c69cdf736f7f0bba1b10066e
Joar <joar.sabel@netikka.fi>
2022-10-27 15:50:33 +0300

    Broken-video-player

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,34 @@
-<script>
+<script lang="ts">
 	import Counter from './Counter.svelte';
 	import welcome from '$lib/images/svelte-welcome.webp';
 	import welcome_fallback from '$lib/images/svelte-welcome.png';
+	import {
+		Player,
+		Video, 
+		DefaultUi,
+		usePlayerStore,
+	} from '@vime/svelte';
+
+	const source: string = 'src/lib/videos/video.mp4'
+	const poster: string = 'src/lib/images/strommingPog.png'
+
+	// Custom UI component.
+	import TapSidesToSeek from './TapSidesToSeek.svelte';
+
+	// Obtain a ref if you need to call any methods.
+	//let player: Player;
+
+	/**
+	 * All player properties are available through the store. If you prefer, you could also pass 
+	 * properties directly to the player and listen for events.
+	 */
+	//const { paused } = usePlayerStore(() => player);
+
+	const onPlaybackReady = () => {
+		// ...
+	};
+
+	//$: console.log($paused);
 </script>
 
 <svelte:head>
@@ -17,15 +44,18 @@
 				<img src={welcome_fallback} alt="Welcome" />
 			</picture>
 		</span>
-
-		to your new<br />SvelteKit app
 	</h1>
 
-	<h2>
-		try editing <strong>src/routes/+page.svelte</strong>
-	</h2>
-
-	<Counter />
+	<div id="container">
+		<Player controls>
+			<Video crossOrigin="" poster={poster}>
+				<source data-src={source} type="video/mp4">
+			</Video>
+	
+			<DefaultUi noClickToPlay>
+			</DefaultUi>
+		</Player>
+	</div>
 </section>
 
 <style>
@@ -55,5 +85,10 @@
 		height: 100%;
 		top: 0;
 		display: block;
+	}
+
+	#container {
+		width: 100%;
+		max-width: 960px;
 	}
 </style>
